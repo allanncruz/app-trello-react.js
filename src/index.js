91fea337046f99526e705fdbaf8f98575f77e7e4
Allan Dantas <allancruz716@gmail.com>
2018-08-29 14:48:06 -0300

    Creating architecture
    Settings

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from'react-dom'
+import { Provider } from 'react-redux'
+import { Route } from 'react-router'
+import { ConnectedRouter } from 'react-router-redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import Home from './containers/Home'
+
+import cfg from './store'
+
+const store = cfg.configStore()
+
+ReactDOM.render(
+    <main id="main" className="container-fluid">
+        <Provider store={ store }>
+            <ConnectedRouter history = { cfg.history } >
+                <Route exact path="/" component={ Home } />
+            </ConnectedRouter>
+        </Provider>
+    </main>,
+document.getElementById('root')
+)
