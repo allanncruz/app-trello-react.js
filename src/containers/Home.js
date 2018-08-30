dd65edcb2bfd6958812bc7290c6d9a92ce3071aa
Allan Dantas <allancruz716@gmail.com>
2018-08-30 00:50:57 -0300

    Define header

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import './Home.scss'
 
 class Home extends Component {
     constructor(props) {
@@ -7,7 +8,28 @@ class Home extends Component {
 
     render() {
         return (
-            <h1>Kanban Border React.js Redux</h1>
+            <div>
+                <nav id="navbar-center" className="navbar navbar-default navbar-fixed-top">
+                    <div className="container-fluid" >
+                        <div className="navbar-header">
+                            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar">
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                                <span className="icon-bar"></span>
+                            </button>
+                            <a href="" className="navbar-brand">
+                                Trello React.js
+                            </a>
+                        </div>
+                        <div className="navbar-collapse collapse" id="navbar">
+                            <ul className="nav navbar-nav">
+
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+                <h1>Kanban Border React.js Redux</h1>
+            </div>
         )
     }
 }
